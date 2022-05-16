76a682d29c486b0b74a62c9b5a0ead008a456bef
Alabhya Jindal <alabhya10@gmail.com>
2022-05-16 11:49:35 +0530

    Completed Player Controls.

--- pages/Player.js
+++ pages/Player.js
@@ -1,12 +1,37 @@
 import Image from 'next/image';
+import { HeartIcon } from '@heroicons/react/solid';
+import { Shuffle } from 'phosphor-react';
+import { Rewind } from 'phosphor-react';
+import { Play } from 'phosphor-react';
+import { FastForward } from 'phosphor-react';
+import { Repeat } from 'phosphor-react';
 
 export default function Player() {
   return (
-    <div className='p-4 bg-zinc-900 flex items-center gap-4'>
-      <Image src='/hypnocurrency.jpg' height={60} width={60} />
-      <div>
-        <h2 className='text-white font-medium text-sm'>Hypnocurrency</h2>
-        <p className='text-slate-300 text-xs'>Rezz, deadmau5</p>
+    <div className='p-4 bg-zinc-900 flex items-center gap-14'>
+      {/* Current audio */}
+      <div className=' flex items-center gap-4'>
+        <Image src='/hypnocurrency.jpg' height={60} width={60} />
+        <div>
+          <h2 className='text-white font-medium text-sm'>Hypnocurrency</h2>
+          <p className='text-slate-300 text-xs'>Rezz, deadmau5</p>
+        </div>
+        <HeartIcon className='h-5 text-green-500' />
+      </div>
+      {/* Player controls */}
+      <div className='flex flex-col gap-4 items-center'>
+        <div className='flex gap-2 items-center'>
+          <Shuffle size={24} className='text-green-500' />
+          <Rewind size={24} className='text-slate-300' />
+          <Play size={32} className='text-slate-300' />
+          <FastForward size={24} className='text-slate-300' />
+          <Repeat size={24} className='text-slate-300' />
+        </div>
+        <div className='flex items-center gap-2'>
+          <p className='text-xs text-slate-300'>0:00</p>
+          <div className='h-1 w-[400px] bg-zinc-600 rounded-lg'></div>
+          <p className='text-xs text-slate-300'>4:52</p>
+        </div>
       </div>
     </div>
   );
--- pages/Sidebar.js
+++ pages/Sidebar.js
@@ -21,35 +21,37 @@ export default function Sidebar() {
       <div className='mt-6'>
         <div className='flex gap-4 items-center'>
           <HomeIcon className='h-6 text-slate-300' />
-          <h2 className='text-slate-300 font-bold text-sm'>Home</h2>
+          <h2 className='text-slate-300 font-medium text-sm'>Home</h2>
         </div>
         <div className='flex gap-4 items-center mt-4'>
           <SearchIcon className='h-6 text-slate-300' />
-          <h2 className='text-slate-300 font-bold text-sm'>Search</h2>
+          <h2 className='text-slate-300 font-medium text-sm'>Search</h2>
         </div>
         <div className='flex gap-4 items-center mt-4'>
           <CollectionIcon className='h-6 text-slate-300' />
-          <h2 className='text-slate-300 font-bold text-sm'>Your Library</h2>
+          <h2 className='text-slate-300 font-medium text-sm'>Your Library</h2>
         </div>
       </div>
 
       <div className='mt-10'>
         <div className='flex gap-4 items-center'>
           <PlusIcon className='h-6 text-black bg-slate-300 p-1 rounded-sm' />
-          <h2 className='text-slate-300 font-bold text-sm'>Create Playlist</h2>
+          <h2 className='text-slate-300 font-medium text-sm'>
+            Create Playlist
+          </h2>
         </div>
         <div className='flex gap-4 items-center mt-4'>
           <HeartIcon className='h-6 text-slate-300 bg-purple-600 p-1 rounded-sm' />
-          <h2 className='text-slate-300 font-bold text-sm'>Liked Songs</h2>
+          <h2 className='text-slate-300 font-medium text-sm'>Liked Songs</h2>
         </div>
         <div className='flex gap-4 items-center mt-4'>
           <BookmarkIcon className='h-6 text-green-500 bg-green-900 p-1 rounded-sm' />
-          <h2 className='text-slate-300 font-bold text-sm'>Your Episodes</h2>
+          <h2 className='text-slate-300 font-medium text-sm'>Your Episodes</h2>
         </div>
       </div>
 
       <div className='border-t border-slate-700 mt-4 mb-4'></div>
-      <ul className='text-slate-300 text-sm font-medium flex flex-col gap-4'>
+      <ul className='text-slate-300 text-sm flex flex-col gap-4'>
         <li>Ambient Relaxation</li>
         <li>Electronic Focus</li>
         <li>House Focus</li>
